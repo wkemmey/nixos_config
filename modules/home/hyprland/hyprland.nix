b5121b8709135796e4de3d76d0ebc66543580b5a
dwilliam62 <don.e.williams@gmail.com>
2025-04-02 20:42:22 +0000

    Edit hyprland.nix Add options for vrr/fr vrr can cause issues with NVIDIA and AMD GPUs set vrr to 0 if screen goes black momentarily randomly or when you fullscreen an app

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -105,6 +105,10 @@ in {
         disable_hyprland_logo = true;
         disable_splash_rendering = true;
         enable_swallow = false;
+        vfr = true;   # Variable Frame Rate 
+        vrr = 2;   #Variable Refresh Rate  Might need to set to 0 for NVIDIA/AQ_DRM_DEVICES
+        # Screen flashing to black momentarily or going black when app is fullscreen
+        # Try setting vrr to 0
       };  
 
 
