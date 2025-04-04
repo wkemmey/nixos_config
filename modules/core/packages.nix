e4cc98be04e2dfddb6668cd54ce9b58f3024dced
dwilliam62 <don.e.williams@gmail.com>
2025-04-04 01:16:48 +0000

    packages.nix added glxinfo for GPU info inxi -G needs that to show GPU info

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -32,6 +32,7 @@
     file-roller # Archive Manager
     gedit # Simple Graphical Text Editor
     gimp # Great Photo Editor
+    glxinfo # Needed for inxi -G GPU info
     greetd.tuigreet # The Login Manager (Sometimes Referred To As Display Manager)
     htop # Simple Terminal Based System Monitor
     hyprpicker # Color Picker
