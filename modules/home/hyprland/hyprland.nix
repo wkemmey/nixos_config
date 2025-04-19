2a5f2bbe6a27e0392d0867f6e0282b38cd2cd7b8
dwilliam62 <don.e.williams@gmail.com>
2025-04-19 07:59:50 +0000

    hyprland.nix added wl-paste to capture txt/imagaes It now will store images and text for use by SUPER + V rofi menu search and copy text and images

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -46,6 +46,8 @@ in {
     };
     settings = {
       exec-once = [
+        "wl-paste --type text --watch cliphist store # Stores only text data"
+        "wl-paste --type image --watch cliphist store # Stores only image data"
         "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user start hyprpolkitagent"
