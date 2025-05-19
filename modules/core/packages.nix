decc6e5e15d82f74f837b054f4b5852ecead1230
dwilliam62 <don.e.williams@gmail.com>
2025-05-19 14:32:13 +0000

    Edit packages.nix added a few more utils

--- modules/core/packages.nix
+++ modules/core/packages.nix
@@ -7,6 +7,7 @@
     firefox.enable = false; # Firefox is not installed by default
     dconf.enable = true;
     seahorse.enable = true;
+    hyprland.enable = true;  #create desktop file and depedencies if you switch to GUI login MGR
     hyprlock.enable = true;  #resolve pam issue https://gitlab.com/Zaney/zaneyos/-/issues/164
     fuse.userAllowOther = true;
     mtr.enable = true;
@@ -22,18 +23,22 @@
   environment.systemPackages = with pkgs; [
     amfora # Fancy Terminal Browser For Gemini Protocol
     appimage-run # Needed For AppImage Support
+    bottom # btop like util
     brave # Brave Browser
     brightnessctl # For Screen Brightness Control
     cmatrix # Matrix Movie Effect In Terminal
     cowsay # Great Fun Terminal Program
     docker-compose # Allows Controlling Docker From A Single File
     duf # Utility For Viewing Disk Usage In Terminal
+    dysk # disk usage util
     eza # Beautiful ls Replacement
     ffmpeg # Terminal Video / Audio Editing
     file-roller # Archive Manager
+    gdu # graphical disk usage
     gedit # Simple Graphical Text Editor
     gimp # Great Photo Editor
     glxinfo # Needed for inxi -G GPU info
+    gpijng #graphical ping 
     greetd.tuigreet # The Login Manager (Sometimes Referred To As Display Manager)
     htop # Simple Terminal Based System Monitor
     hyprpicker # Color Picker
@@ -55,10 +60,12 @@
     rhythmbox
     ripgrep # Improved Grep
     socat # Needed For Screenshots
+    sox  # audio support for FFMPEG
     unrar # Tool For Handling .rar Files
     unzip # Tool For Handling .zip Files
     usbutils # Good Tools For USB Devices
     v4l-utils # Used For Things Like OBS Virtual Camera
+    waypaper # backup wallpaper GUI
     wget # Tool For Fetching Files With Links
     yazi #TUI File Manager
     ytmdl # Tool For Downloading Audio From YouTube
