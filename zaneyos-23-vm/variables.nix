53f2aaa83136ce8cb1fc1727888c1fbe80a484b1
Tyler Kelley <tylerzanekelley@gmail.com>
2025-05-28 14:48:31 -0500

    Added test host for v2.3 branch
     On branch stable-2.3
     Your branch is up to date with 'origin/stable-2.3'.
    
     Changes to be committed:
    	new file:   zaneyos-23-vm/default.nix
    	new file:   zaneyos-23-vm/hardware.nix
    	new file:   zaneyos-23-vm/host-packages.nix
    	new file:   zaneyos-23-vm/variables.nix

---
+++ zaneyos-23-vm/variables.nix
@@ -0,0 +1,46 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "Tyler Kelley";
+  gitEmail = "tylerzanekelley@gmail.com";
+
+  # Hyprland Settings
+  extraMonitorSettings = "
+    monitor=Virtual-1,1920x1080@60,auto,1
+    ";
+
+  # Waybar Settings
+  clock24h = false;
+
+  # Program Options
+  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+  consoleKeyMap = "us";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+
+  # Enable NFS
+  enableNFS = true;
+
+  # Enable Printing Support
+  printEnable = false;
+
+  # Set Stylix Image
+  stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
+
+  # Set Waybar
+  # Includes alternates such as waybar-curved.nix & waybar-ddubs.nix
+  waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
+
+  # Set Animation style
+  # Available options are:
+  # animations-def.nix  (default)
+  # animations-end4.nix (end-4 project)
+  # animations-dynamic.nix (ml4w project)
+  animChoice = ../../modules/home/hyprland/animations-def.nix;
+
+  # Enable Thunar GUI File Manager
+  thunarEnable = true;
+}
