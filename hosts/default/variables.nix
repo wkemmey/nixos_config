499953db1f8b2b78adf4d58d282432e57be8481a
Don Williams <don.e.williams@gmail.com>
2025-06-20 03:18:55 -0500

    Upd default variables.nix add new waybar
     On branch stable-2.3
     Your branch is up to date with 'origin/stable-2.3'.
    
     Changes to be committed:
    	modified:   hosts/default/variables.nix
    	new file:   modules/home/waybar/waybar-nekodyke.nix

--- hosts/default/variables.nix
+++ hosts/default/variables.nix
@@ -1,19 +1,21 @@
 {
   # Git Configuration ( For Pulling Software Repos )
-  gitUsername = "Tyler Kelley";
-  gitEmail = "tylerzanekelley@gmail.com";
+  gitUsername = "Don Williams";
+  gitEmail = "don.e.williams@gmail.com";
 
   # Hyprland Settings
-  # ex: extraMonitorSettings = "monitor = Virtual-1,1920x1080@60,auto,1";
-  # ex: extraMonitorSettings = "monitor = HDMI-A-1,1920x1080@60,auto,1";
-  extraMonitorSettings = "";
+  # ex "monitor=HDMI-A-1, 1920x1080@60,auto,1"
+  #
+  extraMonitorSettings = "
+    monitor = Virtual-1, 1920x1080@60,auto,1
+    ";
 
   # Waybar Settings
   clock24h = false;
 
   # Program Options
-  browser = "brave"; # Set Default Browser (google-chrome-stable for google-chrome)
-  terminal = "kitty"; # Set Default System Terminal
+  browser = "google-chrome-stable"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "ghostty"; # Set Default System Terminal
   keyboardLayout = "us";
   consoleKeyMap = "us";
 
@@ -25,27 +27,31 @@
   enableNFS = true;
 
   # Enable Printing Support
-  printEnable = false;
+  printEnable = true;
 
   # Set Stylix Image
   stylixImage = ../../wallpapers/AnimeGirlNightSky.jpg;
 
   # Set Waybar
-  #  Available Options:
+  # Includes alternates such as:
+  # Just uncomment the one you want and comment out the others
+
+  #waybarChoice = ../../modules/home/waybar/Jerry-waybars.nix;
   #waybarChoice = ../../modules/home/waybar/waybar-simple.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-curved.nix;
+  #waybarChoice = ../../modules/home/waybar/waybar-nekodyke.nix;
   #waybarChoice = ../../modules/home/waybar/waybar-ddubs.nix;
-  #waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
-  #waybarChoice = ../../modules/home/waybar/Jerry-waybar.nix;
-  waybarChoice = ../../modules/home/waybar/waybar-curved.nix;
+  waybarChoice = ../../modules/home/waybar/waybar-ddubs-2.nix;
 
   # Set Animation style
   # Available options are:
-  # animations-def.nix  (default)
+  # animations-def.nix  (standard)
   # animations-end4.nix (end-4 project)
   # animations-dynamic.nix (ml4w project)
   # animations-moving.nix (ml4w project)
-  animChoice = ../../modules/home/hyprland/animations-def.nix;
+  # Just change the name after the - and rebuild
+  animChoice = ../../modules/home/hyprland/animations-end4.nix;
 
   # Enable Thunar GUI File Manager
-  thunarEnable = false;
+  thunarEnable = true;
 }
