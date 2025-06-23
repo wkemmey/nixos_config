d0d21ee6e34608703086605b067011d0878f65dc
Don Williams <don.e.williams@gmail.com>
2025-06-22 20:53:33 -0500

    Disabled SDDM until variable to enable is done
     On branch ddubs-dev
     Your branch is up to date with 'origin/ddubs-dev'.
    
     Changes to be committed:
    	modified:   core/default.nix

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -4,17 +4,17 @@
     ./doas.nix
     ./flatpak.nix
     ./fonts.nix
-    #./greetd.nix
+    ./greetd.nix
     ./hardware.nix
     ./network.nix
     ./nfs.nix
     ./nh.nix
     ./packages.nix
     ./printing.nix
-    ./sddm.nix
+    #./sddm.nix    #Disabled for now until can set up per host variables
     ./security.nix
     ./services.nix
-    ./starfish.nix
+    #./starfish.nix  Not sure why this is here, as it's in modules/home
     ./steam.nix
     ./stylix.nix
     ./syncthing.nix
