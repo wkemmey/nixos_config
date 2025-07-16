48253539c887cb0d74b4ee17d854882dd97e97c6
dwilliam62 <don.e.williams@gmail.com>
2025-07-16 15:10:42 +0000

    Edit fonts.nix symbola fonts won't build 404 error https://github.com/nixos/nixpkgs/issues/425166 Waiting for fix commented it out for now

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -6,7 +6,8 @@
       noto-fonts-emoji
       noto-fonts-cjk-sans
       font-awesome
-      symbola
+      #symbola  Fails to build 7/16/25 waiting on fix
+      # https://github.com/nixos/nixpkgs/issues/425166
       material-icons
       fira-code
       fira-code-symbols
