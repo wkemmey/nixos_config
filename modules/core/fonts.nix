274e8663b282fe6d90799a5abf171c937d31934f
Don Williams <don.e.williams@gmail.com>
2025-07-25 13:16:24 -0500

    Syntax error in fonts.nix
     On branch stable-2.3
     Your branch is ahead of 'origin/stable-2.3' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   modules/core/fonts.nix

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -5,14 +5,6 @@
       noto-fonts-cjk-sans
       font-awesome
       symbola
-      Fails
-      to
-      build
-      7/16/25
-      waiting
-      on
-      fix
-      # https://github.com/nixos/nixpkgs/issues/425166
       material-icons
       fira-code
       fira-code-symbols
