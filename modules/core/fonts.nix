e50eea7fff6e8087b32f5aca3c1b040055f9a0ec
Don Williams <don.e.williams@gmail.com>
2025-07-28 15:58:48 -0500

    Added fonts and updated starship config
     On branch stable-2.3
     Your branch is up to date with 'origin/stable-2.3'.
    
     Changes to be committed:
    	modified:   modules/core/fonts.nix
    	modified:   modules/home/starship.nix

--- modules/core/fonts.nix
+++ modules/core/fonts.nix
@@ -1,15 +1,17 @@
 {pkgs, ...}: {
   fonts = {
     packages = with pkgs; [
+      dejavu_fonts
+      fira-code
+      fira-code-symbols
+      noto-fonts
       noto-fonts-emoji
       noto-fonts-cjk-sans
       font-awesome
-      #symbola
-      material-icons
-      fira-code
-      fira-code-symbols
       jetbrains-mono
+      material-icons
       maple-mono.NF
+      #symbola   #still 404 error
       terminus_font
     ];
   };
