653b5aef6b333f0da322fbb605b22b8d609b6c0e
Don Williams <don.e.williams@gmail.com>
2025-08-18 14:50:31 -0400

    imported emacs nix files

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,7 +1,6 @@
 { host, ... }:
 let
-  inherit
-    (import ../../hosts/${host}/variables.nix)
+  inherit (import ../../hosts/${host}/variables.nix)
     alacrittyEnable
     ghosttyEnable
     tmuxEnable
@@ -12,75 +11,52 @@ let
     ;
 in
 {
-  imports =
-    [
-      ./amfora.nix
-      ./bash.nix
-      ./bashrc-personal.nix
-      ./bat.nix
-      ./btop.nix
-      ./bottom.nix
-      ./cava.nix
-      ./emoji.nix
-      ./eza.nix
-      ./fastfetch
-      ./fzf.nix
-      ./gh.nix
-      ./git.nix
-      ./gtk.nix
-      ./htop.nix
-      ./hyprland
-      ./kitty.nix
-      ./lazygit.nix
-      ./obs-studio.nix
-      ./nvf.nix
-      ./obs-studio.nix
-      ./rofi
-      ./qt.nix
-      ./scripts
-      ./scripts/gemini-cli.nix
-      #./starship.nix
-      #./starship-ddubs-1.nix
-      ./stylix.nix
-      ./swappy.nix
-      ./swaync.nix
-      ./tealdeer.nix
-      ./virtmanager.nix
-      waybarChoice
-      ./wlogout
-      ./xdg.nix
-      ./yazi
-      ./zoxide.nix
-      ./zsh
-    ]
-    ++ (
-      if helixEnable
-      then [ ./evil-helix.nix ]
-      else [ ]
-    )
-    ++ (
-      if vscodeEnable
-      then [ ./vscode.nix ]
-      else [ ]
-    )
-    ++ (
-      if weztermEnable
-      then [ ./wezterm.nix ]
-      else [ ]
-    )
-    ++ (
-      if ghosttyEnable
-      then [ ./ghostty.nix ]
-      else [ ]
-    )
-    ++ (
-      if tmuxEnable
-      then [ ./tmux.nix ]
-      else [ ]
-    )
-    ++ (
-      if alacrittyEnable
-      then [ ./alacritty.nix ]
-      else [ ]
-    );
+  imports = [
+    ./amfora.nix
+    ./bash.nix
+    ./bashrc-personal.nix
+    ./bat.nix
+    ./btop.nix
+    ./bottom.nix
+    ./cava.nix
+    ./editors/doom-emacs.nix
+    ./editors/doom-emacs-install.nix
+    ./emoji.nix
+    ./eza.nix
+    ./fastfetch
+    ./fzf.nix
+    ./gh.nix
+    ./git.nix
+    ./gtk.nix
+    ./htop.nix
+    ./hyprland
+    ./kitty.nix
+    ./lazygit.nix
+    ./obs-studio.nix
+    ./nvf.nix
+    ./obs-studio.nix
+    ./rofi
+    ./qt.nix
+    ./scripts
+    ./scripts/gemini-cli.nix
+    #./starship.nix
+    #./starship-ddubs-1.nix
+    ./stylix.nix
+    ./swappy.nix
+    ./swaync.nix
+    ./tealdeer.nix
+    ./virtmanager.nix
+    waybarChoice
+    ./wlogout
+    ./xdg.nix
+    ./yazi
+    ./zoxide.nix
+    ./zsh
+  ]
+  ++ (if helixEnable then [ ./evil-helix.nix ] else [ ])
+  ++ (if vscodeEnable then [ ./vscode.nix ] else [ ])
+  ++ (if weztermEnable then [ ./wezterm.nix ] else [ ])
+  ++ (if ghosttyEnable then [ ./ghostty.nix ] else [ ])
+  ++ (if tmuxEnable then [ ./tmux.nix ] else [ ])
+  ++ (if alacrittyEnable then [ ./alacritty.nix ] else [ ]);
 }
