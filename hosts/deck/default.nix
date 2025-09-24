18b97988d69da102601a158f7211ff1325864f9b
theblackdon <theblackdonatello@gmail.com>
2025-09-24 13:55:19 -0400

    Remove deck host configuration and update nh.nix

--- hosts/deck/default.nix
+++
@@ -1,9 +0,0 @@
-{...}: {
-  imports = [
-    ./hardware.nix
-    ./host-packages.nix
-  ];
-
-  # Enable ly display manager by default
-  services.displayManager.ly.enable = true;
-}
--- hosts/deck/host-packages.nix
+++
@@ -1,9 +0,0 @@
-{pkgs, ...}: {
-  environment.systemPackages = with pkgs; [
-    # Add your host-specific packages here
-    # Example packages from nixos-leno:
-    # audacity
-    # discord
-    # nodejs
-  ];
-}
