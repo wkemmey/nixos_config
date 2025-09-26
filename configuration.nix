8d63b98b1c46e1c6179a86a9612a11faf2a4b985
wkemmey <whit@fastmail.com>
2025-09-26 17:51:40 -0400

    Update

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,9 @@
   # or disable the firewall altogether
   #networking.firewall.enable = false;
 
+  #hardware.bluetooth.enable = true;
+  #services.blueman.enable = true;
+
   #### system settings ####
 
   time.timeZone = "America/New_York";
@@ -51,6 +54,12 @@
     LC_TELEPHONE = "en_US.UTF-8";
     LC_TIME = "en_US.UTF-8";
   };
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    nerd-fonts.caskaydia-mono
+  ];
 
   #### services ####
 
@@ -78,14 +87,17 @@
 
   #services.printing.enable = true;
 
-  #services.pulseaudio.enable = false;
-  #security.rtkit.enable = true;
-  #services.pipewire = {
-  #  enable = true;
-  #  alsa.enable = true;
-  #  alsa.support32Bit = true;
-  #  pulse.enable = true;
-  #};
+  #programs.direnv.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
 
   #### user accounts ####
 
@@ -101,10 +113,59 @@
   #### system packages
 
   environment.systemPackages = with pkgs; [
-    fish
     git
     wayland-utils
-    rofi
+    #rofi-wayland
+
+    # for hyprland
+    hyprshot
+    hyprpicker
+    hyprsunset
+    brightnessctl
+    pamixer
+    playerctl
+    #gnome-themes-extra
+    pavucntrol
+
+    # for system
+    libnotify
+    nautilus
+    alejandra
+    blueberry
+    clipse
+    fzf
+    zoxide
+    ripgrep
+    eza
+    fd
+    curl
+    unzip
+    wget
+    gnumake
+
+    # tuis
+    lazygit
+    #lazydocker
+    btop
+    powertop
+    fastfetch
+
+    # guis
+    chromium
+    obsidian
+    vlc
+    #signal-desktop
+    dropbox
+
+    # development tools
+    github-desktop
+    gh
+
+    # containers
+    #docker-compose
+    #ffmpeg
+    #typora
+    
   ];
 
   #### programs ####
@@ -112,6 +173,12 @@
   programs.hyprland.enable = true;
 
   #programs.firefox.enable = true;
+
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = ["${mySettings.username}"];
+  };
 
   # I use zsh btw
   #environment.shells = with pkgs; [ fish ];
@@ -128,10 +195,10 @@
   # This section provides a minimal working config. You should customize it.
   # The configuration is written in the Hyprland format, not Nix.
   environment.etc."hypr/hyprland.conf".text = ''
-    # Monitor setup
+    # monitor setup
     monitor=,preferred,auto,1
 
-    # Main keyboard and mouse binds
+    # main keyboard and mouse binds
     $mainMod = SUPER
 
     bind = $mainMod, Q, exec, fish
@@ -140,35 +207,54 @@
     bind = $mainMod, E, exec, rofi -show drun
     bind = $mainMod, V, togglefloating,
 
-    # Window management
+    # window management
     bind = $mainMod, P, pseudo, # dwindle
     bind = $mainMod, J, togglesplit, # dwindle
 
-    # Move focus
+    # move focus
     bind = $mainMod, H, movefocus, l
     bind = $mainMod, L, movefocus, r
     bind = $mainMod, K, movefocus, u
     bind = $mainMod, J, movefocus, d
 
-    # Move windows
+    # move windows
     bind = $mainMod SHIFT, H, movewindow, l
     bind = $mainMod SHIFT, L, movewindow, r
     bind = $mainMod SHIFT, K, movewindow, u
     bind = $mainMod SHIFT, J, movewindow, d
     
-    # Workspace switching
+    # workspace switching
     bind = $mainMod, 1, workspace, 1
     bind = $mainMod, 2, workspace, 2
     bind = $mainMod, 3, workspace, 3
     bind = $mainMod, 4, workspace, 4
 
-    # Move windows to workspaces
+    # move windows to workspaces
     bind = $mainMod SHIFT, 1, movetoworkspace, 1
     bind = $mainMod SHIFT, 2, movetoworkspace, 2
     bind = $mainMod SHIFT, 3, movetoworkspace, 3
     bind = $mainMod SHIFT, 4, movetoworkspace, 4
 
-    # ... and many more
+    # applications
+    bind = $mainMod, A, exec, $webapp=https://chatgpt.com"
+    bind = $mainMod SHIFT, A, exec, $webapp=https://grok.com"
+    bind = $mainMod, C, exec, $webapp=https://app.hey.com/calendar/weeks/"
+    bind = $mainMod, E, exec, $webapp=https://app.hey.com"
+    bind = $mainMod, Y, exec, $webapp=https://youtube.com/"
+    bind = $mainMod SHIFT, G, exec, $webapp=https://web.whatsapp.com/"
+    bind = $mainMod, X, exec, $webapp=https://x.com/"
+    bind = $mainMod SHIFT, X, exec, $webapp=https://x.com/compose/post"
+
+    bind = $mainMod, return, exec, $terminal"
+    bind = $mainMod, F, exec, $fileManager"
+    bind = $mainMod, B, exec, $browser"
+    bind = $mainMod, M, exec, $music"
+    bind = $mainMod, N, exec, $terminal -e nvim"
+    bind = $mainMod, T, exec, $terminal -e btop"
+    bind = $mainMod, D, exec, $terminal -e lazydocker"
+    bind = $mainMod, G, exec, $messenger"
+    bind = $mainMod, O, exec, obsidian -disable-gpu"
+    bind = $mainMod, slash, exec, $passwordManager"
   '';
 
 }
