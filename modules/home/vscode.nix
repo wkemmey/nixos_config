087f775b3204284772fde527e698f96eb5a3e43d
whit <whit@fastmail.com>
2025-11-20 21:26:47 -0500

    Update

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -1,13 +1,9 @@
-{pkgs, ...}: {
+{ pkgs, lib, ... }:
+{
   nixpkgs.config.allowUnfree = true;
 
   programs.vscode = {
     enable = true;
-    # Prefer the Catppuccin Mocha color theme; stylix will also attempt to
-    # generate a matching theme when `stylix.targets.vscode.enable = true`.
-    settings = {
-      "workbench.colorTheme" = "Catppuccin Mocha";
-    };
     profiles = {
       default = {
         extensions = with pkgs.vscode-extensions; [
@@ -22,4 +18,26 @@
       };
     };
   };
+
+  # Provide a default VSCode settings template that will be copied into the
+  # user's config only if the real file does not already exist. This avoids
+  # attempting to set a non-existent `programs.vscode.settings` option and
+  # preserves any user customizations.
+  home.file.".config/Code/User/settings.json.template" = {
+    text = builtins.toJSON {
+      "workbench.colorTheme" = "Catppuccin Mocha";
+    };
+  };
+
+  home.activation.vscodeSettingsInit = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    SETTINGS_FILE="$HOME/.config/Code/User/settings.json"
+    TEMPLATE_FILE="$HOME/.config/Code/User/settings.json.template"
+
+    if [ ! -f "$SETTINGS_FILE" ] || [ -L "$SETTINGS_FILE" ]; then
+      $DRY_RUN_CMD rm -f "$SETTINGS_FILE"
+      $DRY_RUN_CMD cp "$TEMPLATE_FILE" "$SETTINGS_FILE"
+      $DRY_RUN_CMD chmod 644 "$SETTINGS_FILE"
+    fi
+  '';
+}
 }
