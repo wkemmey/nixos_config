573e75c43f8913fbdca8456454dc043d4a3f3c9b
whit <whit@fastmail.com>
2025-11-20 21:42:45 -0500

    Update

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -11,25 +11,25 @@ lib.mkIf stylixEnable {
   stylix = {
     enable = true;
     image = stylixImage;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    # base16Scheme = {
-    #   base00 = "282936";
-    #   base01 = "3a3c4e";
-    #   base02 = "4d4f68";
-    #   base03 = "626483";
-    #   base04 = "62d6e8";
-    #   base05 = "e9e9f4";
-    #   base06 = "f1f2f8";
-    #   base07 = "f7f7fb";
-    #   base08 = "ea51b2";
-    #   base09 = "b45bcf";
-    #   base0A = "00f769";
-    #   base0B = "ebff87";
-    #   base0C = "a1efe4";
-    #   base0D = "62d6e8";
-    #   base0E = "b45bcf";
-    #   base0F = "00f769";
-    # };
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = {
+      base00: "1e1e2e"; # base
+      base01: "181825"; # mantle
+      base02: "313244"; # surface0
+      base03: "45475a"; # surface1
+      base04: "585b70"; # surface2
+      base05: "cdd6f4"; # text
+      base06: "f5e0dc"; # rosewater
+      base07: "b4befe"; # lavender
+      base08: "f38ba8"; # red
+      base09: "fab387"; # peach
+      base0A: "f9e2af"; # yellow
+      base0B: "a6e3a1"; # green
+      base0C: "94e2d5"; # teal
+      base0D: "89b4fa"; # blue
+      base0E: "cba6f7"; # mauve
+      base0F: "f2cdcd"; # flamingo
+    };
     polarity = "dark";
     opacity.terminal = 1.0;
     cursor = {
