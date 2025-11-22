e654df885afe00431addd58d311de3f569f40ff6
whit <whit@fastmail.com>
2025-11-21 22:10:55 -0500

    Update

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -7,20 +7,23 @@
 
   programs.vscode = {
     enable = true;
-
-    profiles = {
-      default = {
-        extensions = with pkgs.vscode-extensions; [
-          jeff-hykin.better-nix-syntax
-          rust-lang.rust-analyzer
-          vadimcn.vscode-lldb
-          tamasfe.even-better-toml
-          usernamehw.errorlens
-        ];
-        # Note: All font sizes (including chat/markdown preview) are managed by Stylix
-        # via the vscode.enable target in modules/home/stylix.nix
-      };
-    };
+    
+    # Allow VSCode to manage extensions and settings mutably
+    # This prevents conflicts with Stylix trying to manage the same files
+    mutableExtensionsDir = true;
+    
+    # Let Stylix handle all theming through userSettings
+    # Don't use profiles when Stylix is managing VSCode
+    extensions = with pkgs.vscode-extensions; [
+      jeff-hykin.better-nix-syntax
+      rust-lang.rust-analyzer
+      vadimcn.vscode-lldb
+      tamasfe.even-better-toml
+      usernamehw.errorlens
+    ];
+    
+    # Note: All font sizes and colors are managed by Stylix
+    # via the vscode.enable target in modules/home/stylix.nix
   };
 }
 #"editor.semanticTokenColorCustomizations": {
