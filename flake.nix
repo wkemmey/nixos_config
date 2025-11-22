8f3abf2aab87771b2d181bdf60adbb1593d8b2c8
whit <whit@fastmail.com>
2025-11-22 16:22:56 -0500

    Update

--- flake.nix
+++ flake.nix
@@ -19,7 +19,6 @@
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.quickshell.follows = "quickshell";
     };
     helium-browser = {
       url = "github:fpletz/flake";
