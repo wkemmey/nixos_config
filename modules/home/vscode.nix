c80f011df8ec31809ccce01c447dd87607ecd998
whit <whit@fastmail.com>
2025-11-22 16:07:39 -0500

    Update

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -16,8 +16,6 @@
     # Both can coexist in profiles without conflict
     profiles.default = {
       extensions = with pkgs.vscode-extensions; [
-        GitHub.copilot
-        GitHub.copilot-chat
         jeff-hykin.better-nix-syntax
         rust-lang.rust-analyzer
         vadimcn.vscode-lldb
