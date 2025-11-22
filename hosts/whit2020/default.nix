61e19e08553cc418bbebb34dc02edd6cbd39a472
whit <whit@fastmail.com>
2025-11-22 14:03:56 -0500

    Update

--- hosts/whit2020/default.nix
+++ hosts/whit2020/default.nix
@@ -5,10 +5,6 @@
     ./host-packages.nix
   ];
 
-  # Use greetd + tuigreet (TUI) to launch Niri. Disable other DMs.
-  services.displayManager.sddm.enable = false;
-  services.displayManager.ly.enable = false;
-
   # Keep niri available at system level and ensure session package is present
   programs.niri.package = pkgs.niri;
   services.displayManager.sessionPackages = [ pkgs.niri ];
