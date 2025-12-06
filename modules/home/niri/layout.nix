f9bf3a995efd7b539fe2b7d08b3bd68fdcd24c4a
whit <whit@fastmail.com>
2025-12-05 22:15:04 -0500

    Update

--- modules/home/niri/layout.nix
+++ modules/home/niri/layout.nix
@@ -31,10 +31,10 @@
       }
 
       focus-follows-mouse {
-        # Set the maximum scroll allowed to trigger focus-follows-mouse.
-        # 5% is a good starting point. This prevents jumping to completely 
-        # off-screen windows, but allows minor scrolling for partially visible ones.
-        max-scroll-amount = "5%"; 
+        // Set the maximum scroll allowed to trigger focus-follows-mouse.
+        // 5 percent is a good starting point. This prevents jumping to completely 
+        // off-screen windows, but allows minor scrolling for partially visible ones.
+        max-scroll-amount "5%"
       }
       warp-mouse-to-focus
   }
