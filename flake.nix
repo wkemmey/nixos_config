4868b44a3a9abd2e44c14e9d3475a9fdd63f633c
whit <whit@fastmail.com>
2025-12-06 12:25:54 -0500

    Move to 25.11 from unstable

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Black Don OS (Based on ZaneyOS)";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nvf.url = "github:notashelf/nvf";
