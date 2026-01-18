afaf8f974b066160383d51e156c3e8ca6c5ee64f
wkemmey <whit@fastmail.com>
2026-01-18 18:12:51 -0500

    Fixes boot warnings/info.

--- hosts/whit2020/default.nix
+++ hosts/whit2020/default.nix
@@ -6,5 +6,6 @@
   ];
 
   # disable amd_pstate driver; ryzen 9 3900 (zen 2) uses acpi-cpufreq
-  boot.kernelParams = [ "amd_pstate.enable=0" ];
+  # set loglevel=4 to suppress debug messages and non-critical warnings during boot
+  boot.kernelParams = [ "amd_pstate.enable=0" "loglevel=4" ];
 }
